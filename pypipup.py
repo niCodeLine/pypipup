c0e30cb9204a6e7a6efa1efc5b879799ace576fd
Nico Spok <nicospok@hotmail.com>
2024-04-11 01:08:08 -0600

    Add files via upload

--- pypipup.py
+++ pypipup.py
@@ -3,11 +3,11 @@ import subprocess
 def update_pips():
     try:
         print('> Updating pip...')
-        subprocess.call('pip install --upgrade pip', shell=True)
+        subprocess.call('pip3 install --upgrade pip', shell=True)
         print('Pip updated!')
 
         print('> Checking for outdated packages...')
-        subprocess.call('pip list -o > outdated_pips.txt', shell=True)
+        subprocess.call('pip3 list -o > outdated_pips.txt', shell=True)
 
         out_pips_file = 'outdated_pips.txt'
         print('- Updating packages...')
@@ -17,12 +17,12 @@ def update_pips():
                 package_name = line.strip().split(' ')[0]
                 if index > 1:
                     print(f'> Updating {package_name}...')
-                    subprocess.call(f'pip install {package_name} --upgrade', shell=True)
+                    subprocess.call(f'pip3 install {package_name} --upgrade', shell=True)
 
         print('Packages updated')
 
         print('> Checking un-updated pips...')
-        subprocess.call('pip list -o > outdated_pips.txt', shell=True)
+        subprocess.call('pip3 list -o > outdated_pips.txt', shell=True)
         with open(out_pips_file, 'r') as f:
             content = f.read()
             if not content:
